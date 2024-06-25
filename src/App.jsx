f0c0fa6d4dfcf42e9460e83743782bb637895f45
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-06-25 18:45:22 +0300

    banner comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Cart from "./components/Cart";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
       <Cart />
+      <Banner />
     </>
   );
 }
