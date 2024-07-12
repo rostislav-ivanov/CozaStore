3a3f4dfd4661c87de34f97a05191925be99178db
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-07-12 10:30:37 +0300

    CartModal - read data form context and display

--- client/src/components/cartModal/CartModal.jsx
+++ client/src/components/cartModal/CartModal.jsx
@@ -1,4 +1,11 @@
-export default function CartModal({ showCart, hideCartHendler }) {
+import { useContext } from "react";
+import { BagContext } from "../../context/bagContext";
+import { Link } from "react-router-dom";
+
+export default function CartModal({ hideCartHendler }) {
+  const { bag } = useContext(BagContext);
+  const total = bag.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
   return (
     <div className="wrap-header-cart js-panel-cart show-header-cart">
       <div className="s-full js-hide-cart"></div>
@@ -15,82 +22,66 @@ export default function CartModal({ showCart, hideCartHendler }) {
           </div>
         </div>
 
-        <div className="header-cart-content flex-w js-pscroll">
-          <ul className="header-cart-wrapitem w-full">
-            <li className="header-cart-item flex-w flex-t m-b-12">
-              <div className="header-cart-item-img">
-                <img src="/images/item-cart-01.jpg" alt="IMG" />
-              </div>
-
-              <div className="header-cart-item-txt p-t-8">
-                <a
-                  href="#"
-                  className="header-cart-item-name m-b-18 hov-cl1 trans-04"
-                >
-                  White Shirt Pleat
-                </a>
-
-                <span className="header-cart-item-info">1 x $19.00</span>
-              </div>
-            </li>
-
-            <li className="header-cart-item flex-w flex-t m-b-12">
-              <div className="header-cart-item-img">
-                <img src="/images/item-cart-02.jpg" alt="IMG" />
-              </div>
-
-              <div className="header-cart-item-txt p-t-8">
-                <a
-                  href="#"
-                  className="header-cart-item-name m-b-18 hov-cl1 trans-04"
-                >
-                  Converse All Star
-                </a>
-
-                <span className="header-cart-item-info">1 x $39.00</span>
-              </div>
-            </li>
-
-            <li className="header-cart-item flex-w flex-t m-b-12">
-              <div className="header-cart-item-img">
-                <img src="/images/item-cart-03.jpg" alt="IMG" />
-              </div>
-
-              <div className="header-cart-item-txt p-t-8">
-                <a
-                  href="#"
-                  className="header-cart-item-name m-b-18 hov-cl1 trans-04"
-                >
-                  Nixon Porter Leather
-                </a>
-
-                <span className="header-cart-item-info">1 x $17.00</span>
-              </div>
-            </li>
-          </ul>
-
-          <div className="w-full">
+        {bag.length === 0 && (
+          <div className="header-cart-content flex-w js-pscroll">
             <div className="header-cart-total w-full p-tb-40">
-              Total: $75.00
-            </div>
-
-            <div className="header-cart-buttons flex-w w-full">
-              <a
-                href="shoping-cart.html"
-                className="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10"
-              >
-                View Cart
-              </a>
-
-              <a
-                href="shoping-cart.html"
-                className="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10"
-              >
-                Check Out
-              </a>
+              Your cart is empty!
             </div>
           </div>
-        </div>
+        )}
+
+        {bag.length > 0 && (
+          <div className="header-cart-content flex-w js-pscroll">
+            <ul className="header-cart-wrapitem w-full">
+              {bag.map((item) => (
+                <li
+                  key={item.id}
+                  className="header-cart-item flex-w flex-t m-b-12"
+                >
+                  <div className="header-cart-item-img">
+                    <img src={item.image} alt="IMG" />
+                  </div>
+
+                  <div className="header-cart-item-txt">
+                    <Link
+                      to={`/details/${item._id}`}
+                      className="header-cart-item-name hov-cl1 trans-04"
+                      onClick={hideCartHendler}
+                    >
+                      {item.name}
+                    </Link>
+                    <span className="header-cart-item-info">
+                      Color: {item.color}
+                    </span>
+                    <span className="header-cart-item-info">
+                      Size: {item.size}
+                    </span>
+
+                    <span className="header-cart-item-info">
+                      {item.quantity} x ${Number(item.price).toFixed(2)}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+            <div className="w-full">
+              <div className="header-cart-total w-full p-tb-40">
+                Total: ${Number(total).toFixed(2)}
+              </div>
+
+              <div className="header-cart-buttons flex-w w-full">
+                <Link
+                  to="/checkout"
+                  className="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10"
+                  onClick={hideCartHendler}
+                >
+                  Check Out
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
