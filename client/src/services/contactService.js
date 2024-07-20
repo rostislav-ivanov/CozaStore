98094101468e4ae42f07c987e1fad26b4b6ac8ca
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-07-20 16:59:22 +0300

    Footer - users can be able to subscribe for Newsletter

--- client/src/services/contactService.js
+++ client/src/services/contactService.js
@@ -1,8 +1,8 @@
-const baseUrl = "http://localhost:3030/jsonstore/messages";
+const baseUrl = "http://localhost:3030/jsonstore";
 
 export async function sendMessage(message) {
   try {
-    const response = await fetch(`${baseUrl}`, {
+    const response = await fetch(`${baseUrl}/messages`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -18,5 +18,27 @@ export async function sendMessage(message) {
     return data;
   } catch (error) {
     throw new Error(`Failed to send message: ${error.message}`);
+  }
+}
+
+export async function subscribe(email) {
+  try {
+    const response = await fetch(`${baseUrl}/subscribe`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(`Failed to subscribe: ${error.message}`);
   }
 }
